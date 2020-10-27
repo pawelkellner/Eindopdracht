bfd82ae05e8baa298e49df5c04fdd88c48445ebb
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2020-10-27 15:15:02 +0100

    Eerste commit

---
+++ Eindopdracht/README.md
@@ -0,0 +1,1 @@
+Dit is mijn eindopdracht voor de tekst based applicatie
---
+++ Eindopdracht/eindopdracht.py
@@ -0,0 +1,22 @@
+import os, time
+
+#def verhaalstukje01():
+
+gameRunning = True
+
+print("--------------------------------------------------------------------------------")
+print("Hallo! Dit is mijn vluchteling Simulator!")
+print("De bedoling van het spel is dat je ervaart hoe het voelt om een vluchteling \nte zijn.")
+print("Het spel werkt als volgt. Je hebt meer keuzen vragen en aan de hand wat je hebt geantwoordt, krijg je andere eindes en scenario's.")
+print("--------------------------------------------------------------------------------")
+time.sleep(1)
+print("\n\n\nAls je wilt beginnen klick op 'ENTER'!")
+
+answer0 = input()
+
+if answer0 == "":
+    print("DD")
+
+os.system("clear")
+
+#while gameRunning == True:
