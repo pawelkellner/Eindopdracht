6f9f69f368b5857fa8e1a5088b41e066cf0756c9
Pawel <smrpawel@gmail.com>
2020-10-28 12:26:53 +0100

    klein begin

--- Eindopdracht/eindopdracht.py
+++ Eindopdracht/eindopdracht.py
@@ -1,11 +1,15 @@
 import os, time
 
-#def verhaalstukje01():
+def beginverhaal():
+    print("Je wordt wakker... Zoals altijd hoor je schoten en bommen in de achtergrond.")
+    
 
 gameRunning = True
 
+loader = 0
+
 print("--------------------------------------------------------------------------------")
-print("Hallo! Dit is mijn vluchteling Simulator!")
+print("Hallo! Dit is mijn Vluchteling Simulator!")
 print("De bedoling van het spel is dat je ervaart hoe het voelt om een vluchteling \nte zijn.")
 print("Het spel werkt als volgt. Je hebt meer keuzen vragen en aan de hand wat je hebt geantwoordt, krijg je andere eindes en scenario's.")
 print("--------------------------------------------------------------------------------")
@@ -17,6 +21,19 @@ answer0 = input()
 if answer0 == "":
     print("DD")
 
-os.system("clear")
+os.system("cls")
 
-#while gameRunning == True:
+while loader < 4:
+    print("Aan het laden.")
+    time.sleep(0.5)
+    os.system("cls")
+    print("Aan het laden..")
+    time.sleep(0.5)
+    os.system("cls")
+    print("Aan het laden...")
+    time.sleep(0.5)
+    os.system("cls")
+    loader = loader + 1
+
+while gameRunning == True:
+    beginverhaal()
